3758b7d62862a770e6b2a5fa451610fc1ba68751
Gil Raphaelli <gil@elastic.co>
2018-02-07 15:31:20 -0500

    add tiny and huge profiles

--- profile/tiny.go
+++ profile/tiny.go
@@ -5,7 +5,7 @@ import (
 )
 
 func init() {
-	Register("small", []target.Target{
+	Register("tiny", []target.Target{
 		{"POST", "v1/errors", target.V1Error1, 3, 10},
 		{"POST", "v1/errors", target.V1Error2, 1, 10},
 		{"POST", "v1/transactions", target.V1Transaction2, 50, 10},
