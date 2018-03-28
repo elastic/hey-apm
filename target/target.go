eba02708475f6ddd62b259143616c089a0133c2d
Gil Raphaelli <gil@elastic.co>
2018-03-28 12:04:44 -0400

    compress requests by default

--- target/target.go
+++ target/target.go
@@ -1,13 +1,14 @@
 package target
 
 import (
+	"bytes"
+	"compress/gzip"
 	"io/ioutil"
 	"math"
 	"net/http"
+	"time"
 
 	"github.com/graphaelli/hey/requester"
-	"bytes"
-	"time"
 )
 
 // Config holds global work configuration
@@ -50,6 +51,20 @@ func (targets Targets) GetWork(baseUrl string, cfg *Config) []*requester.Work {
 
 		report := ioutil.Discard
 		body := bytes.Replace(t.Body, []byte("2018-01-09T03:35:37.604813Z"), []byte(time.Now().UTC().Format(time.RFC3339)), -1)
+
+		if !cfg.DisableCompression {
+			var b bytes.Buffer
+			gz := gzip.NewWriter(&b)
+			if _, err := gz.Write([]byte(body)); err != nil {
+				panic(err)
+			}
+			if err := gz.Close(); err != nil {
+				panic(err)
+			}
+			body = b.Bytes()
+			req.Header.Add("Content-Encoding", "gzip")
+		}
+
 		work[i] = &requester.Work{
 			Request:            req,
 			RequestBody:        body,
