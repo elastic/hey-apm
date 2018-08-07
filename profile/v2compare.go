9e4887f819ae1d1192dd4a5a17245fffc7cfeddb
Gil Raphaelli <gil@elastic.co>
2018-08-07 17:03:39 -0400

    allow env vars SPANS and FRAMES to configure v2comp payload

--- profile/v2compare.go
+++ profile/v2compare.go
@@ -3,17 +3,30 @@ package profile
 import (
 	"bytes"
 	"encoding/json"
+	"os"
+	"strconv"
 
 	"github.com/elastic/hey-apm/compose"
 	"github.com/elastic/hey-apm/target"
 )
 
-const (
+var (
 	numSpans  = 10
 	numFrames = 10
 )
 
 func init() {
+	if spansEnv, ok := os.LookupEnv("SPANS"); ok {
+		if n, err := strconv.Atoi(spansEnv); err == nil {
+			numSpans = n
+		}
+	}
+	if framesEnv, ok := os.LookupEnv("FRAMES"); ok {
+		if n, err := strconv.Atoi(framesEnv); err == nil {
+			numFrames = n
+		}
+	}
+
 	// make a span with frames
 	singleSpan := compose.V2Span(numFrames)
 	spans := make([][]byte, numSpans)
