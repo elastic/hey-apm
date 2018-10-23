24c1356d7d182cf8b23d594126a7f34ce3d0042f
jalvz <juan.alvarez@elastic.co>
2018-10-23 15:09:55 +0200

    Fix V2 request body composition

--- compose/metadata.go
+++ compose/metadata.go
@@ -1,15 +1,3 @@
 package compose
 
-var Metadata = []byte(`{
-"metadata": {
-	"user": {"id": "123", "email": "s@test.com", "username": "john"}, 
-	"process": {"ppid": 6789, "pid": 1234,"argv": ["node", "server.js"], "title": "node"}, 
-	"system": {"platform": "darwin", "hostname": "prod1.example.com", "architecture": "x64"}, 
-	"service":{
-		"name": "backendspans", 
-		"language": {"version": "8", "name": "ecmascript"}, 
-		"agent": {"version": "3.14.0", "name": "elastic-node"}, 
-		"environment": "staging", 
-		"framework": {"version": "1.2.3", "name": "Express"}, 
-		"version": "5.1.3", 
-		"runtime": {"version": "8.0.0", "name": "node"}}}}`)
+var Metadata = []byte(`{"metadata": {	"user": {"id": "123", "email": "s@test.com", "username": "john"}, 	"process": {"ppid": 6789, "pid": 1234,"argv": ["node", "server.js"], "title": "node"}, 	"system": {"platform": "darwin", "hostname": "prod1.example.com", "architecture": "x64"}, 	"service":{		"name": "backendspans", 		"language": {"version": "8", "name": "ecmascript"}, 		"agent": {"version": "3.14.0", "name": "elastic-node"}, 		"environment": "staging", 		"framework": {"version": "1.2.3", "name": "Express"}, 		"version": "5.1.3", 		"runtime": {"version": "8.0.0", "name": "node"}}}}`)
