ff4beb90b919155f60f51e6104274112585ff817
jalvz <juan.alvarez@elastic.co>
2018-10-23 09:33:53 +0200

    Remove v1 from `compose` package and update readme

--- compose/stacktrace.go
+++ compose/stacktrace.go
@@ -1,3 +1,14 @@
 package compose
 
-var StacktraceFrame = []byte(`{ "function": "onread", "abs_path": "net.js", "filename": "net.js", "lineno": 547, "library_frame": true, "vars": { "key": "value" }, "module": "some module", "colno": 4, "context_line": "line3", "pre_context": [ "  var trans = this.currentTransaction", "" ], "post_context": [ "    ins.currentTransaction = prev", "    return result"] }`)
+var StacktraceFrame = []byte(`{
+"function": "onread", 
+"abs_path": "net.js", 
+"filename": "net.js", 
+"lineno": 547, 
+"library_frame": true, 
+"vars": {"key": "value"}, 
+"module": "some module", 
+"colno": 4, 
+"context_line": "line3", 
+"pre_context": [ "  var trans = this.currentTransaction", "" ], 
+"post_context": [ "    ins.currentTransaction = prev", "    return result"]}`)
