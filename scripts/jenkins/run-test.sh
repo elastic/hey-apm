6cbf65da2699b1fd246022082b04bd13db488d2e
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-03 17:57:41 +0100

    Add more details about what's required

--- scripts/jenkins/run-test.sh
+++ scripts/jenkins/run-test.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+# Requirements
+#   env variable WORKSPACE
+#   env variable APM_SERVER_DIR
+#   env variable CLOUD_ADDR
+#   env variable CLOUD_USERNAME
+#   env variable CLOUD_PASSWORD
+#
 set -exuo pipefail
 
-export GOPATH=$WORKSPACE
+export GOPATH=$WORKSPACE/build
 export PATH=$PATH:$GOPATH/bin
 
 if [ ! -d "$APM_SERVER_DIR" ] ; then
@@ -9,10 +16,16 @@ if [ ! -d "$APM_SERVER_DIR" ] ; then
   exit 1
 fi
 
-eval "$(gvm ${GO_VERSION})"
-echo "Installing hey-apm dependencies and running unit tests..."
-echo "Running apm-server stress tests..."
 set +x
+# https://github.com/moovweb/gvm/issues/188
+[[ -s "$GVM_ROOT/scripts/gvm" ]] && source "$GVM_ROOT/scripts/gvm"
+eval "$(gvm use ${GO_VERSION})"
+
+echo "Installing hey-apm dependencies"
+go get -v -u github.com/t-yuki/gocover-cobertura
+go get -v -u github.com/jstemmer/go-junit-report
+
+echo "Running apm-server stress tests..."
 
 export COV_DIR="build/coverage"
 export COV_FILE="${COV_DIR}/hey-apm-stress-test.cov"
@@ -22,6 +35,8 @@ mkdir -p "${COV_DIR}"
 (ELASTICSEARCH_URL=$CLOUD_ADDR \
   ELASTICSEARCH_USR=$CLOUD_USERNAME \
   ELASTICSEARCH_PWD=$CLOUD_PASSWORD \
-  go test -timeout 2h  -v github.com/elastic/hey-apm/server/client 2>&1 | tee ${OUT_FILE}) || echo -e "\033[31;49mTests FAILED\033[0m"
+  go test -timeout 2h  -v github.com/elastic/hey-apm/server/client 2>&1 | tee ${OUT_FILE}) \\
+  && echo -e "\033[31;49mTests PASSED\033[0m"
+  || echo -e "\033[31;49mTests FAILED\033[0m"
 
 go-junit-report < ${OUT_FILE} > build/junit-hey-apm-stress-test-report.xml
--- scripts/jenkins/unit-test.sh
+++ scripts/jenkins/unit-test.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+# Requirements
+#   env variable WORKSPACE
+#   env variable GO_VERSION
+
 set -euo pipefail
 
 # https://github.com/moovweb/gvm/issues/188
 [[ -s "$GVM_ROOT/scripts/gvm" ]] && source "$GVM_ROOT/scripts/gvm"
 
 echo "Setup Go"
-export GOPATH=$WORKSPACE
+export GOPATH=${WORKSPACE}/build
 export PATH=$PATH:$GOPATH/bin
 eval "$(gvm use ${GO_VERSION})"
 
@@ -19,9 +23,14 @@ export COV_FILE="${COV_DIR}/hey-apm.cov"
 export OUT_FILE="build/test-report.out"
 mkdir -p "${COV_DIR}"
 
-(SKIP_EXTERNAL=1 SKIP_STRESS=1 go test -v ./... -coverprofile="${COV_FILE}" 2>&1 | tee ${OUT_FILE}) || echo -e "\033[31;49mTests FAILED\033[0m"
+(SKIP_EXTERNAL=1 SKIP_STRESS=1 go test -v ./... -coverprofile="${COV_FILE}" 2>&1 | tee ${OUT_FILE}) \
+  && echo -e "\033[31;49mTests PASSED\033[0m" \
+  || echo -e "\033[31;49mTests FAILED\033[0m"
 go-junit-report < ${OUT_FILE} > build/junit-hey-apm-report.xml
 
 echo "Running cobertura"
-go tool cover -html="${COV_FILE}" -o "${COV_DIR}/coverage-hey-apm-report.html"
+go tool cover -html="${COV_FILE}" -o "${COV_DIR}/coverage-hey-apm-report.html" \
+  && echo -e "\033[31;49mTests PASSED\033[0m" \
+  || echo -e "\033[31;49mTests FAILED\033[0m"
+
 gocover-cobertura < "${COV_FILE}" > "${COV_DIR}/coverage-hey-apm-report.xml"
