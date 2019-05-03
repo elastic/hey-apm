a991957b1c62f27415f1ab839a256a41e6653652
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-03 17:06:09 +0100

    Restore previous version

--- scripts/jenkins/unit-test.sh
+++ scripts/jenkins/unit-test.sh
@@ -5,6 +5,11 @@ export GOPATH=$WORKSPACE
 export PATH=$PATH:$GOPATH/bin
 eval "$(gvm ${GO_VERSION})"
 echo "Installing hey-apm dependencies and running unit tests..."
+go get -v -u github.com/golang/dep/cmd/dep
+dep ensure -v
+
+go get -v -u github.com/t-yuki/gocover-cobertura
+go get -v -u github.com/jstemmer/go-junit-report
 
 export COV_DIR="build/coverage"
 export COV_FILE="${COV_DIR}/hey-apm.cov"
