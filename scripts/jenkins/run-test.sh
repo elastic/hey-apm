7c31b9f6e8c2cffa448ddafb6ffa72f3f4cafb94
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-03 18:06:04 +0100

    Fix cosmetic issues in the syntax

--- scripts/jenkins/run-test.sh
+++ scripts/jenkins/run-test.sh
@@ -6,7 +6,7 @@
 #   env variable CLOUD_USERNAME
 #   env variable CLOUD_PASSWORD
 #
-set -exuo pipefail
+set -euo pipefail
 
 export GOPATH=$WORKSPACE/build
 export PATH=$PATH:$GOPATH/bin
@@ -16,7 +16,6 @@ if [ ! -d "$APM_SERVER_DIR" ] ; then
   exit 1
 fi
 
-set +x
 # https://github.com/moovweb/gvm/issues/188
 [[ -s "$GVM_ROOT/scripts/gvm" ]] && source "$GVM_ROOT/scripts/gvm"
 eval "$(gvm use ${GO_VERSION})"
@@ -35,8 +34,8 @@ mkdir -p "${COV_DIR}"
 (ELASTICSEARCH_URL=$CLOUD_ADDR \
   ELASTICSEARCH_USR=$CLOUD_USERNAME \
   ELASTICSEARCH_PWD=$CLOUD_PASSWORD \
-  go test -timeout 2h  -v github.com/elastic/hey-apm/server/client 2>&1 | tee ${OUT_FILE}) \\
-  && echo -e "\033[31;49mTests PASSED\033[0m"
+  go test -timeout 2h  -v github.com/elastic/hey-apm/server/client 2>&1 | tee ${OUT_FILE}) \
+  && echo -e "\033[31;49mTests PASSED\033[0m" \
   || echo -e "\033[31;49mTests FAILED\033[0m"
 
 go-junit-report < ${OUT_FILE} > build/junit-hey-apm-stress-test-report.xml
