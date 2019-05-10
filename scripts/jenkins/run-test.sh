b803431413cc6e94f226c9da2bb9747a8b11c236
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-10 10:16:47 +0100

    [APM-CI][hey-apm] Move dependencies to the go modules and comsetic changes in the shell script

--- scripts/jenkins/run-test.sh
+++ scripts/jenkins/run-test.sh
@@ -12,26 +12,22 @@ RED='\033[31;49m'
 GREEN='\033[32;49m'
 NC='\033[0m' # No Color
 
+echo "Setup Go ${GO_VERSION}"
 export GOPATH=$WORKSPACE/build
 export PATH=$PATH:$GOPATH/bin
+eval "$(gvm ${GO_VERSION})"
+
+export COV_DIR="build/coverage"
+export COV_FILE="${COV_DIR}/hey-apm-stress-test.cov"
+export OUT_FILE="build/stress-test.out"
+mkdir -p "${COV_DIR}"
 
 if [ ! -d "$APM_SERVER_DIR" ] ; then
   echo "you need to define APM_SERVER_DIR environment variable pointing to the APM server source code"
   exit 1
 fi
 
-eval "$(gvm ${GO_VERSION})"
-
-echo "Installing hey-apm dependencies"
-go get -v -u github.com/t-yuki/gocover-cobertura
-go get -v -u github.com/jstemmer/go-junit-report
-
 echo "Running apm-server stress tests..."
-
-export COV_DIR="build/coverage"
-export COV_FILE="${COV_DIR}/hey-apm-stress-test.cov"
-export OUT_FILE="build/stress-test.out"
-mkdir -p "${COV_DIR}"
 
 (ELASTICSEARCH_URL=$CLOUD_ADDR \
   ELASTICSEARCH_USR=$CLOUD_USERNAME \
--- scripts/jenkins/unit-test.sh
+++ scripts/jenkins/unit-test.sh
@@ -14,16 +14,12 @@ export GOPATH=${WORKSPACE}/build
 export PATH=$PATH:$GOPATH/bin
 eval "$(gvm ${GO_VERSION})"
 
-echo "Installing hey-apm dependencies"
-go get -v -u github.com/t-yuki/gocover-cobertura
-go get -v -u github.com/jstemmer/go-junit-report
-
-echo "Running unit tests..."
 export COV_DIR="build/coverage"
 export COV_FILE="${COV_DIR}/hey-apm.cov"
 export OUT_FILE="build/test-report.out"
 mkdir -p "${COV_DIR}"
 
+echo "Running unit tests..."
 (SKIP_EXTERNAL=1 SKIP_STRESS=1 go test -v ./... -coverprofile="${COV_FILE}" 2>&1 | tee ${OUT_FILE}) \
   && echo -e "${GREEN}Tests PASSED${NC}" || echo -e "${RED}Tests FAILED${NC}"
 go-junit-report < ${OUT_FILE} > build/junit-hey-apm-report.xml
