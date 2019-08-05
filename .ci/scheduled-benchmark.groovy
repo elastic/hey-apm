cb95788f43dc38db76c7f3281bdd11c0139fa67e
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-08-05 12:53:31 +0100

    it should not execute the benchmark in the MBP but in the pipeline only

--- .ci/scheduled-benchmark.groovy
+++ .ci/scheduled-benchmark.groovy
@@ -26,7 +26,7 @@ pipeline {
     STACK_VERSION = "${params.STACK_VERSION}"
     NOTIFY_TO = credentials('notify-to')
     JOB_GCS_BUCKET = credentials('gcs-bucket')
-    BENCHMARK_SECRET  = 'secret/apm-team/ci/apm-server-benchmark-cloud'
+    BENCHMARK_SECRET  = 'secret/apm-team/ci/java-agent-benchmark-cloud'
   }
   options {
     timeout(time: 1, unit: 'HOURS')
