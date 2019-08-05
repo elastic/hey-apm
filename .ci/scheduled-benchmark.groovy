35a1ff31622cf6b19a0292be35fcf76f9f3c764d
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-08-05 11:39:52 +0100

    ci: specific pipeline to run the benchmarking

--- .ci/scheduled-benchmark.groovy
+++ .ci/scheduled-benchmark.groovy
@@ -1,4 +1,19 @@
-#!/usr/bin/env groovy
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
 @Library('apm@current') _
 
@@ -6,10 +21,9 @@ pipeline {
   agent any
   environment {
     BASE_DIR="src/github.com/elastic/hey-apm"
-    APM_SERVER_BASE_DIR = "src/github.com/elastic/apm-server"
     JOB_GIT_CREDENTIALS = "f6c7695a-671e-4f4f-a331-acdce44ff9ba"
     GO_VERSION = "${params.GO_VERSION}"
-    APM_SERVER_VERSION = "${params.APM_SERVER_VERSION}"
+    STACK_VERSION = "${params.STACK_VERSION}"
     NOTIFY_TO = credentials('notify-to')
     JOB_GCS_BUCKET = credentials('gcs-bucket')
     BENCHMARK_SECRET  = 'secret/apm-team/ci/apm-server-benchmark-cloud'
@@ -21,8 +35,6 @@ pipeline {
     ansiColor('xterm')
     disableResume()
     durabilityHint('PERFORMANCE_OPTIMIZED')
-    rateLimitBuilds(throttle: [count: 60, durationName: 'hour', userBoost: true])
-    quietPeriod(10)
   }
   triggers {
     cron('H H(3-5) * * 1-5')
