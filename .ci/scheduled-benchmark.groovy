4c95fae2e3f7bcc81c86408122568f7bb1c8a435
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-10-30 15:14:28 +0100

    fix: disable temporarily (#147)

--- .ci/scheduled-benchmark.groovy
+++ .ci/scheduled-benchmark.groovy
@@ -80,6 +80,9 @@ pipeline {
         */
         stage('Benchmark') {
           agent { label 'metal' }
+          when {
+            expresion { return false }
+          }
           steps {
             deleteDir()
             unstash 'source'
